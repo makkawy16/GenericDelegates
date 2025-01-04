05b3ae583fec417c621f3058138c4cec868ca785
yousefMakkawy <youssef.elmekkawy@tildetech.ae>
2025-01-05 00:22:21 +0200

    Add project files.

---
+++ GenericDelegates/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace GenericDelegates
+{
+
+    internal class Program
+    {
+        public delegate bool Filter<in T>(T input);
+        static void Main(string[] args)
+        {
+         IEnumerable<int> l1 = new int[] {1,2,3,4};
+            PrintNumbers(l1, x => x <= 2);
+            IEnumerable<decimal> l2 = new decimal[] {1,2,3,4};
+            PrintNumbers(l2, x => x <= 2);
+        }
+        static void PrintNumbers<T>(IEnumerable<T> numbers , Filter<T> filter)
+        {
+            foreach (var i in numbers)
+            {
+                if (filter(i))
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
